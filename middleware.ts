b19985cc41fbcc4311285437c9aeb08bf14d119e
aakinwole <120341598+aakinwole@users.noreply.github.com>
2024-05-11 19:32:16 -0500

    models, types and authentications

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,13 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-export default clerkMiddleware()
+const isProtectedRoute = createRouteMatcher([
+  '/profile(.*)',
+]);
+
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) auth().protect();
+}, { debug: true })
 
 export const config = {
-  // The following matcher runs middleware on all routes
-  // except static assets.
-  matcher: [ '/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
