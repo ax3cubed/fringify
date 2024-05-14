6427fbce73513618822e2d2271c31e9340fa1428
aakinwole <120341598+aakinwole@users.noreply.github.com>
2024-05-13 23:33:07 -0500

    implementing webhooks

--- middleware.ts
+++ middleware.ts
@@ -9,5 +9,5 @@ export default clerkMiddleware((auth, req) => {
 }, { debug: true })
 
 export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: [ '/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
