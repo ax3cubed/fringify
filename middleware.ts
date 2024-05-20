fec81f2cd72370b13458858b5c853cc1446f8c63
aakinwole <120341598+aakinwole@users.noreply.github.com>
2024-05-19 19:07:13 -0500

    restored clerk webhook folder

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,13 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const isProtectedRoute = createRouteMatcher([
-  '/profile(.*)',
-]);
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)'].concat(['/', '/api/webhooks/clerk', '/api/webhooks/stripe']));
 
-export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) auth().protect();
-}, { debug: true })
+export default clerkMiddleware((auth, request) => {
+  if(!isPublicRoute(request)) {
+    auth().protect();
+  }
+});
 
 export const config = {
-  matcher: [ '/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
-};
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+}
