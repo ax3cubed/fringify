c1ae66c4b0e6fa8dbbb9fa7364f9bd56b64c7048
aakinwole <120341598+aakinwole@users.noreply.github.com>
2024-05-23 23:38:40 -0500

    added transformations

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import Header from "@/components/shared/Header";
+import { SearchParamProps, TransformationTypeKey } from "@/types";
+import { transformationTypes } from "@/constants";
+import TransformationForm from "@/components/shared/TransformationForm";
+import { auth } from "@clerk/nextjs/server";
+import { getUserById } from "@/lib/actions/user.actions";
+import { redirect } from "next/navigation";
 
-const AddTransformationTypePage = () => {
+const AddTransformationTypePage = async ({
+  params: { type },
+}: SearchParamProps) => {
+  const { userId } = auth();
+  if (!userId) redirect("/sign-in");
+  const user = await getUserById(userId);
+  const transformation = transformationTypes[type];
   return (
-    <div>AddTransformationTypePage</div>
-  )
-}
+    <>
+      <Header title={transformation.title} subtitle={transformation.subTitle} />
+      <TransformationForm
+        action="Add"
+        type={transformation.type as TransformationTypeKey}
+        creditBalance={user.creditBalance}
+        userId={user._id}
+      />
+    </>
+  );
+};
 
-export default AddTransformationTypePage
+export default AddTransformationTypePage;
---
+++ components/shared/TransformationForm.tsx
@@ -0,0 +1,89 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+import { Form } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { defaultValues, transformationTypes } from "@/constants";
+import { TransformationFormProps } from "@/types";
+import { CustomField } from "./CustomField";
+
+export const formSchema = z.object({
+  title: z.string().min(2).max(50),
+  aspectRatio: z.string().optional(),
+  color: z.string().optional(),
+  prompt: z.string().optional(),
+  publicId: z.string(),
+});
+
+const TransformationForm = ({
+  action,
+  data = null,
+  type,
+  creditBalance,
+  userId,
+  config,
+}: TransformationFormProps) => {
+  const transformationType = transformationTypes[type];
+  const initialValues =
+    data && action === "Update"
+      ? {
+          title: data.title,
+          aspectRatio: data.aspectRatio,
+          color: data.color,
+          prompt: data.prompt,
+          publicId: data.publicId,
+        }
+      : defaultValues;
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialValues,
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <CustomField
+          control={form.control}
+          name="title"
+          render={({ field }) => <Input {...field} className="input-field" />}
+          className="w-full"
+          formLabel="Image Title"
+        />
+        {type === "fill" && (
+          <CustomField
+            name="aspectRatio"
+            control={form.control}
+            render={({ field }) => (
+              <Select>
+                <SelectTrigger className="w-[180px]">
+                  <SelectValue placeholder="Theme" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="light">Light</SelectItem>
+                  <SelectItem value="dark">Dark</SelectItem>
+                  <SelectItem value="system">System</SelectItem>
+                </SelectContent>
+              </Select>
+            )}
+          />
+        )}
+      </form>
+    </Form>
+  );
+};
+
+export default TransformationForm;
